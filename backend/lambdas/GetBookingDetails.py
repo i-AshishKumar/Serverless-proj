13792e8351ad276c0ffd1e0c7b611735a1e173fc
Ashish Kumar Guntipalli <as589490@dal.ca>
2024-06-21 00:02:26 -0300

    Modified GetBookingDetails Lambda

--- backend/lambdas/GetBookingDetails.py
+++ backend/lambdas/GetBookingDetails.py
@@ -1,39 +1,35 @@
 import json
 import boto3
-from botocore.exceptions import ClientError
+
 
 def lambda_handler(event, context):
     dynamodb = boto3.resource('dynamodb')
-
     table = dynamodb.Table('Bookings')
-
-    booking_code = event['currentIntent']['slots']['booking_code']
-
-    try:
-        response = table.get_item(Key={'booking_code': booking_code})
-
-
-        if 'Item' in response:
-            item = response['Item']
-            duration = item['duration']
-            room_number = item['room_number']
-
-            message = f"Your booking details are:\nDuration: {duration} days\nRoom Number: {room_number}"
-
-        else:
-            message = "Sorry, no booking found with the provided booking code."
-
-    except ClientError as e:
-        print(e.response['Error']['Message'])
-        message = "Sorry, there was an error fetching your booking details. Please try again later."
-
-    return {
-        'dialogAction': {
-            'type': 'Close',
-            'fulfillmentState': 'Fulfilled',
-            'message': {
-                'contentType': 'PlainText',
-                'content': message
+    booking_ref = event['sessionState']['intent']['slots']['booking_code']['value']['originalValue']
+    
+    response = table.get_item(Key={'booking_code':booking_ref})
+    item = response['Item']
+    room_num = item['room_number']
+    duration = item['duration']
+    
+    print(item)
+    # return item
+    result = {
+        "sessionState": {
+            "dialogAction": {
+                "type": "Close",
+            },
+            "intent": {
+                "name": "BookingDetailsIntent",
+                "state": "Fulfilled"
             }
-        }
+        },
+        "messages": [
+            {
+                "contentType": "PlainText",
+                "content": f"The room with {booking_ref} is room mumber:{room_num} and the duration of stay is {duration}"
+            }
+        ]
     }
+    
+    return result
