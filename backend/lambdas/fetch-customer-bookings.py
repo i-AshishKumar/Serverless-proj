992a3841246c67a440765824e4037ddc4b0fc036
Ashish Kumar Guntipalli <as589490@dal.ca>
2024-07-19 15:14:49 -0300

    merged DynamoDB for both chat and booking rooms

--- backend/lambdas/fetch-customer-bookings.py
+++ backend/lambdas/fetch-customer-bookings.py
@@ -6,7 +6,6 @@ from boto3.dynamodb.conditions import Attr
 # Initialize a DynamoDB client
 dynamodb = boto3.resource('dynamodb')
 
-# Replace 'YourDynamoDBTableName' with the actual name of your DynamoDB table
 table_name = 'Room_Bookings'
 table = dynamodb.Table(table_name)
 
@@ -18,12 +17,12 @@ def lambda_handler(event, context):
 
         # Query DynamoDB using the GSI for all items with the given email_id
         response = table.query(
-            IndexName='email_id',  # Use the name of your GSI here
+            IndexName='email_id',
             KeyConditionExpression=boto3.dynamodb.conditions.Key('email_id').eq(email_id)
         )
         items = response.get('Items', [])
         
-        # Return all matching records
+        # Returns all matching records
         return {
             'statusCode': 200,
             'body': json.dumps(items)
